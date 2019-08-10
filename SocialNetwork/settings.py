1984a918ec0b454eb19228dfb26dfb7ae0af18f4
karkinirajan1999 <karkinirajan1999@gmail.com>
2019-08-10 12:14:34 +0545

    meankja

--- SocialNetwork/settings.py
+++ SocialNetwork/settings.py
@@ -114,8 +114,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# MEDIA_URL = "/media/"
+
 MEDIA_URL = "/storage/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "storage")
 
