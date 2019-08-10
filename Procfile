93bffde3c18ce01e18fc3068c90229fb0a231f3a
karkinirajan1999 <karkinirajan1999@gmail.com>
2019-08-10 12:19:08 +0545

    message what message i dont know what message is :D

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn SocialNetwork.wsgi
--- SocialNetwork/settings.py
+++ SocialNetwork/settings.py
@@ -114,11 +114,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-# MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 MEDIA_URL = "/storage/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "storage")
