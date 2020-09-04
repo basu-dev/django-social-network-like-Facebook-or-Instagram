3084bf8dc22e9cbd5cf4022f3368e4278e73588a
basu-dev <adbasudev54@gmail.com>
2020-09-04 17:00:12 +0545

    Other Improvements

--- SocialNetwork/settings.py
+++ SocialNetwork/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,4 +126,4 @@ MEDIA_URL = "/media/"
 MEDIA_URL = "/storage/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "storage")
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
