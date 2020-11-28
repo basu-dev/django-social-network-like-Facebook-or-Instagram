5c0c58f437f1e8e871825e0e17a801878ac5b54f
basu-dev <adbasudev54@gmail.com>
2020-11-28 23:58:47 +0545

    whitenoise

--- SocialNetwork/settings.py
+++ SocialNetwork/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = My_SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["sbraven.herokuapp.com"]
 ALLOWED_HOSTS = ["*"]
@@ -111,7 +111,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
